0467a4cbbbc0cf0a736bebcb672a453b22ced925
Justin Bronder <jsbronder@gmail.com>
2013-04-17 20:49:43 -0400

    help:  remove test code

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,4 @@
 
-def test():
-    print get_nick("somenice!alihfe/aoifhe/aoifhe")
-
 def cmd_help(proto, user, channel, args):
     u = user.split('!', 1)[0]
     cmds = proto.factory.commands.keys()
