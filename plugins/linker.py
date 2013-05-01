884a55ab18f4b85df3b19fb793b9f9b289d178ee
Justin Bronder <jsbronder@gmail.com>
2013-05-01 11:59:59 -0400

    linker:  print tweets instead of linking

--- plugins/linker.py
+++ plugins/linker.py
@@ -1,5 +1,4 @@
 import re
-import string
 import types
 import urllib
 
@@ -7,10 +6,33 @@ import requests
 from twisted.python import log
 from bs4 import BeautifulSoup as soup
 
+def tweet(proto, channel, msg_id):
+    try:
+        html = requests.get('https://api.twitter.com/1/statuses/show.json?id=%s' % (msg_id,))
+    except requests.exceptions.RequestException, e:
+        log.err("Couldn't get tweet %s: %s" % (msg_id, str(e)))
+        return
+
+    msg =  "%s (%s) tweeted: %s" % (
+        html.json['user']['name'],
+        html.json['user']['screen_name'],
+        html.json['text'])
+
+    if isinstance(msg, types.UnicodeType):
+        msg = msg.encode('utf-8')
+
+    proto.say(channel, msg)
+
 def passive_linker(proto, channel, user, msg):
     exp = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     urls = re.findall(exp, msg)
     for url in urls:
+        # Handle twitter links separately
+        twitter_url = re.match('http[s]?://(?:www.)?twitter.com/.+/status/([0-9]+)', url)
+        if twitter_url:
+            tweet(proto, channel, twitter_url.group(1))
+            continue
+
         try:
             html = requests.get(url)
         except requests.exceptions.RequestException, e:
