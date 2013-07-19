ee477a551237fce0822d6cce693386a64d206743
Justin Bronder <jsbronder@gmail.com>
2013-07-19 16:19:02 -0400

    linker:  handle http-equiv: refresh and dump shortener
    - Follow links that are using 'http-equiv: refresh'.
    - Dump the shortener as I'm the only one who likes it.  You're welcome
      Rajiv.

--- plugins/linker.py
+++ plugins/linker.py
@@ -1,6 +1,5 @@
 import re
 import types
-import urllib
 
 import requests
 from twisted.python import log
@@ -9,6 +8,7 @@ from bs4 import BeautifulSoup as soup
 def tweet(proto, channel, msg_id):
     try:
         html = requests.get('https://api.twitter.com/1/statuses/show.json?id=%s' % (msg_id,))
+        html.raise_for_status()
     except requests.exceptions.RequestException, e:
         log.err("Couldn't get tweet %s: %s" % (msg_id, str(e)))
         return
@@ -23,7 +23,16 @@ def tweet(proto, channel, msg_id):
 
     proto.say(channel, msg)
 
-def passive_linker(proto, channel, user, msg):
+def meta_redirect(content):
+    redirect = content.find('meta', attrs={'http-equiv': 'Refresh'})
+    if not redirect:
+        redirect = content.find('meta', attrs={'http-equiv': 'refresh'})
+
+    if redirect:
+        url = redirect['content'].split('url=')[1]
+        return url
+
+def passive_linker(proto, channel, _, msg):
     exp = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     urls = re.findall(exp, msg)
     for url in urls:
@@ -33,35 +42,33 @@ def passive_linker(proto, channel, user, msg):
             tweet(proto, channel, twitter_url.group(1))
             continue
 
-        try:
-            html = requests.get(url, verify=False)
-        except requests.exceptions.RequestException, e:
-            log.err(str(e))
-            return str(e)
+        while True:
+            try:
+                html = requests.get(url, verify=False)
+                html.raise_for_status()
+            except requests.exceptions.RequestException, e:
+                log.err(str(e))
+                return str(e)
 
-        if html.headers['content-type'].startswith('image'):
-            title = u'Image'
-        elif html.headers['content-type'].startswith('application/pdf'):
-            title = u'PDF'
-        else:
-            parsed = soup(html.text)
-            title = parsed.title.text
-            title = title.strip()
-            title = title.replace('\n', ' ')
-
-        surl = ''
-        try:
-            isgd = requests.get('http://is.gd/create.php?format=json&url=%s' % (urllib.quote(url),))
-
-            if 'shorturl' in isgd.json:
-                surl = isgd.json['shorturl']
+            if html.headers['content-type'].startswith('image'):
+                return
+            elif html.headers['content-type'].startswith('application/pdf'):
+                return
             else:
-                log.msg("Failed to get shorturl: %s" %
-                    (isgd.json['errormessage'] if 'errormessage' in isgd.json else ''))
-        except requests.exceptions.RequestException, e:
-            log.err(str(e))
+                parsed = soup(html.text)
+                if parsed.title is None:
+                    redirect = meta_redirect(parsed)
+                    if not redirect:
+                        log.err("Couldn't not parse content from %s" % (url,))
+                        return
+                    else:
+                        url = redirect
+                else:
+                    break
 
-        msg = '%s%s%s' % (title, ' @ ' if surl else '',  surl)
+        msg = 'Link: %s' % (parsed.title.text,)
+        msg = msg.strip().replace('\n', ' ')
+
         if isinstance(msg, types.UnicodeType):
             msg = msg.encode('utf-8')
 
