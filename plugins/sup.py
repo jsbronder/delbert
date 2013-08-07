de488b6ac2c91353100850a3697d179167573e52
Joshua Downer <joshua.downer@gmail.com>
2013-08-07 10:32:44 -0400

    sup: fixed bug in function argument
    Previously 'bot' was the function argument when it should have been 'proto'. Also changed the message so that the response is to the users nick.

--- plugins/sup.py
+++ plugins/sup.py
@@ -1,7 +1,7 @@
-def user_joined_cmd_sup(bot, channel, user):
+def user_joined_cmd_sup(proto, channel, user):
     """
     Traditional Dutch greeting.
     """
-    msg = '%s: sup fucko' % user
+    msg = '%s: sup fucko' % proto.factory.get_nick(user)
     proto.say(channel, msg)
 
