ffb05ee39c0d62a2dd7cc32492f915e42d074089
Joshua Downer <joshua.downer@gmail.com>
2013-08-07 10:40:05 -0400

    sup: using global function to get user nick

--- plugins/sup.py
+++ plugins/sup.py
@@ -2,6 +2,6 @@ def user_joined_cmd_sup(proto, channel, user):
     """
     Traditional Dutch greeting.
     """
-    msg = '%s: sup fucko' % proto.factory.get_nick(user)
+    msg = '%s: sup fucko' % get_nick(user)
     proto.say(channel, msg)
 
