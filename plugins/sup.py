48717e3a3cdde35e08f4a1ff39bbc31b906fbc89
Joshua Downer <joshua.downer@gmail.com>
2013-09-17 13:49:35 -0400

    Made greeting more appropriate

--- plugins/sup.py
+++ plugins/sup.py
@@ -2,6 +2,11 @@ def user_joined_cmd_sup(proto, channel, user):
     """
     Traditional Dutch greeting.
     """
-    msg = '%s: sup fucko' % get_nick(user)
+    nick = get_nick(user)
+    if nick == 'ammorrison':
+        msg = '%s: getting sick of your shit' % nick
+    else:
+        msg = '%s: sup fucko' % nick
+
     proto.say(channel, msg)
 
