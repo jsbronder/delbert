cf2b186383319437fdb89413c26beb2e4c0ae108
Justin Bronder <jsbronder@gmail.com>
2013-10-15 18:17:58 -0400

    linker:  expand twitter links

--- plugins/linker.py
+++ plugins/linker.py
@@ -2,21 +2,38 @@ import re
 import types
 
 import requests
+import requests_oauthlib
 from twisted.python import log
 from bs4 import BeautifulSoup as soup
 
+twitter_auth = None
+
 def tweet(proto, channel, msg_id):
+    if twitter_auth is None:
+        auth_url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+        try:
+            auth = requests_oauthlib.OAuth1(
+                    config['app_key'],
+                    config['app_secret'],
+                    config['user_token'],
+                    config['user_secret'])
+        except Exception, e:
+            log.err("Couldn't authenticate with twitter: %s " % (str(e),))
+            return
+        global twitter_auth
+        twitter_auth = auth
+        
     try:
-        html = requests.get('https://api.twitter.com/1/statuses/show.json?id=%s' % (msg_id,))
+        html  = requests.get('https://api.twitter.com/1.1/statuses/show/%s.json' % (msg_id,), auth=twitter_auth)
         html.raise_for_status()
     except requests.exceptions.RequestException, e:
         log.err("Couldn't get tweet %s: %s" % (msg_id, str(e)))
         return
 
     msg =  "%s (%s) tweeted: %s" % (
-        html.json['user']['name'],
-        html.json['user']['screen_name'],
-        html.json['text'])
+        html.json()['user']['name'],
+        html.json()['user']['screen_name'],
+        html.json()['text'])
 
     if isinstance(msg, types.UnicodeType):
         msg = msg.encode('utf-8')
