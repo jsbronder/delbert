f40935563311e4d28e7efb96622b5f08132397d7
Justin Bronder <jsbronder@gmail.com>
2013-11-09 22:41:29 -0500

    startup:  port to new plugin class
    and add unittest

--- plugins/startup.py
+++ plugins/startup.py
@@ -1,18 +1,31 @@
-import types
-
 import requests
 from twisted.python import log
 
-def cmd_startup(proto, _, channel, __):
-    try:
-        html = requests.get('http://itsthisforthat.com/api.php?text', verify=False)
-    except requests.exceptions.RequestException, e:
-        log.err(str(e))
-        return str(e)
+class Startup(Plugin):
+    def __init__(self, config={}):
+        super(Startup, self).__init__('startup')
+        self._config = config
 
-    msg = html.content.lower().capitalize()
-    if isinstance(msg, types.UnicodeType):
-        msg = msg.encode('utf-8')
+    def query_startup(self):
+        """
+        Query the startup generator.
 
-    proto.say(channel, msg)
+        @return  - a pretty sweet business idea bro!
+        """
+        try:
+            html = requests.get('http://itsthisforthat.com/api.php?text', verify=False)
+        except requests.exceptions.RequestException, e:
+            log.err(str(e))
+            return
+
+        return html.content.lower().capitalize()
+
+    @irc_command('generate startup ideas')
+    def startup(self, user, channel, args):
+        msg = self.query_startup()
+
+        if channel == self.nickname:
+            self._proto.send_msg(get_nick(user), msg)
+        else:
+            self._proto.send_msg(channel, msg)
 
---
+++ test/startup_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+import base
+
+class StartupTester(unittest.TestCase):
+    def setUp(self):
+        self._startup = base.load_plugin('startup.py', 'Startup')
+        self._proto = base.TestProto([self._startup])
+
+    def test_query(self):
+        m = self._startup.query_startup()
+        self.assertTrue(m.startswith('So, basically, it'))
+
+    def test_msg(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, '!startup')
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertTrue(self._proto.msgs[0][2].startswith('So, basically, it'))
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
