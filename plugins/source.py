2279aaef7a28373ddb4cd7ed4b549d4b6d3ceccf
Justin Bronder <jsbronder@gmail.com>
2013-11-10 01:20:25 -0500

    plugins:  add source plugin
    Sometimes you just want to refer to the source, othertimes you just want
    to make sure the bot can refer people correctly.

---
+++ plugins/source.py
@@ -0,0 +1,74 @@
+import os
+import random
+import re
+
+from twisted.python import log
+
+class Source(Plugin):
+    def __init__(self, config={}, seed=None):
+        """
+        Create a referrer to the source.
+
+        @param config   - configuration.
+                            url: path the bot source.
+                            pre_verbs: list of verbs to match before nickname
+                            post_verbs: list of verbs to match after nickname
+                            responses: list of responses
+        """
+        super(Source, self).__init__('source')
+        self._source = config.get('url', 'https://github.com/jsbronder/delbert')
+
+        post_verbs = [
+            'should',
+            'needs',
+        ]
+        pre_verbs = [
+            'make',
+            'modify',
+            'update',
+        ]
+        self._pre_verbs = config.get('pre_verbs', pre_verbs)
+        self._post_verbs = config.get('post_verbs', post_verbs)
+
+        responses = [
+            'Put a little effort in jerky',
+            'Oh yeah?  Bring it right here',
+            'Show me butthole',
+            'I bet you could teach me something like that',
+            'Sigh',
+            'Step up or shut up',
+        ]
+        self._responses = config.get('responses', responses)
+
+        self._re = None
+
+        if seed:
+            random.seed(seed)
+        else:
+            random.seed()
+
+    def initialize(self, nickname, proto):
+        super(Source, self).initialize(nickname, proto)
+        pre = '|'.join(self._pre_verbs)
+        post = '|'.join(self._post_verbs)
+        self._re = re.compile('(^|(%s)\s+|\s+)%s($|\s+|\s+(%s))' % (
+                pre, nickname, post))
+
+    @irc_passive('help user with feature request')
+    def request(self, user, channel, msg):
+        if self._re is None:
+            return
+
+        search = self._re.search(msg)
+        if search is not None:
+            to = get_nick(user) if channel == self.nickname else channel
+            self._proto.send_msg(
+                    to,
+                    '%s, %s' % (
+                        random.choice(self._responses),
+                        self._source))
+
+    @irc_command('show the suggestion box')
+    def source(self, user, channel, args):
+        to = get_nick(user) if channel == self.nickname else channel
+        self._proto.send_msg(to, self._source)
---
+++ test/source_test.py
@@ -0,0 +1,68 @@
+import os
+import unittest
+
+import base
+
+class SourceTester(unittest.TestCase):
+    def setUp(self):
+        self.config = {
+            'responses': ['response'],
+            'url' : 'http://url.com',
+            'pre_verbs': ['make', 'modify'],
+            'post_verbs': ['should'],
+        }
+
+        self._plugin = base.load_plugin('source.py', 'Source', config=self.config, seed=0)
+        self._proto = base.TestProto([self._plugin])
+
+    def test_passive(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, '%s should blah' % (base.TEST_NICK,))
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertEqual(self._proto.msgs[0],
+            ('msg', base.TEST_CHANNEL, '%s, %s' % (self.config['responses'][0], self.config['url'])))
+
+    def test_passive2(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, 'blah %s should blah' % (base.TEST_NICK,))
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertEqual(self._proto.msgs[0],
+            ('msg', base.TEST_CHANNEL, '%s, %s' % (self.config['responses'][0], self.config['url'])))
+
+    def test_passive3(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, 'make %s do' % (base.TEST_NICK,))
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertEqual(self._proto.msgs[0],
+            ('msg', base.TEST_CHANNEL, '%s, %s' % (self.config['responses'][0], self.config['url'])))
+
+    def test_passive4(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, 'make %s' % (base.TEST_NICK,))
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertEqual(self._proto.msgs[0],
+            ('msg', base.TEST_CHANNEL, '%s, %s' % (self.config['responses'][0], self.config['url'])))
+
+    def test_no_passive(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, 'make blah%s' % (base.TEST_NICK,))
+        self.assertEqual(0, len(self._proto.msgs))
+
+    def test_no_passive2(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, 'blah%s should' % (base.TEST_NICK,))
+        self.assertEqual(0, len(self._proto.msgs))
+
+    def test_no_passive3(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, '%sblah should' % (base.TEST_NICK,))
+        self.assertEqual(0, len(self._proto.msgs))
+
+    def test_no_passive4(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, 'make %sblah' % (base.TEST_NICK,))
+        self.assertEqual(0, len(self._proto.msgs))
+
+    def cmd(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, '!source')
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertEqual(self._proto.msgs[0], ('msg', base.TEST_CHANNEL, self.config['url']))
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
