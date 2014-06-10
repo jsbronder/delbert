2bac3148c21b74c8d4c796b9471d65e2e860b0f2
Justin Bronder <jsbronder@gmail.com>
2014-06-10 19:25:27 -0400

    test: skip weather tests that require an api key

--- test/weather_test.py
+++ test/weather_test.py
@@ -3,45 +3,55 @@ import unittest
 
 import base
 
+API_FILE = os.path.join(
+    os.path.realpath(os.path.dirname(__file__)),
+    'db', 'weather-api-key')
+
 class WeatherTester(unittest.TestCase):
     def setUp(self):
-        cdir = os.path.realpath(os.path.dirname(__file__))
         config = {
-            'api_key': open(os.path.join(cdir, 'db', 'weather-api-key')).read().strip(),
+            'api_key': open(API_FILE).read().strip(),
         }
         self._plugin = base.load_plugin('weather.py', 'Weather', config=config)
         self._proto = base.TestProto([self._plugin])
         self._boston = '/q/zmw:02101.1.99999'
         self._ip = '209.6.43.0'
 
+    @unittest.skipIf(not os.path.exists(API_FILE), 'No wunderground api key file found')
     def test_autocomplete(self):
         m = self._plugin.autocomplete('boston')
         self.assertEqual(m, self._boston)
 
+    @unittest.skipIf(not os.path.exists(API_FILE), 'No wunderground api key file found')
     def test_get_weather(self):
         m = self._plugin.get_weather(self._boston)
         loc = m['current_observation']['display_location']['full']
         self.assertEqual(loc, 'Boston, MA')
 
+    @unittest.skipIf(not os.path.exists(API_FILE), 'No wunderground api key file found')
     def test_geoip(self):
         m = self._plugin.geoip(self._ip)
         self.assertEqual(m, ('MA', 'Somerville'))
 
+    @unittest.skipIf(not os.path.exists(API_FILE), 'No wunderground api key file found')
     def test_msg(self):
         self._proto.privmsg('tester', base.TEST_CHANNEL, '!weather boston')
         self.assertEqual(1, len(self._proto.msgs))
         self.assertTrue(self._proto.msgs[0][2].startswith('Boston, MA, '))
 
+    @unittest.skipIf(not os.path.exists(API_FILE), 'No wunderground api key file found')
     def test_host(self):
         self._proto.privmsg('tester!blah@%s' % (self._ip,), base.TEST_CHANNEL, '!weather')
         self.assertEqual(1, len(self._proto.msgs))
         self.assertTrue(self._proto.msgs[0][2].startswith('Somerville, MA, '))
 
+    @unittest.skipIf(not os.path.exists(API_FILE), 'No wunderground api key file found')
     def test_masked_host(self):
         self._proto.privmsg('tester!blah@some/mask', base.TEST_CHANNEL, '!weather')
         self.assertEqual(1, len(self._proto.msgs))
         self.assertTrue(self._proto.msgs[0][2].endswith('IT\'S GONNA RAIN!'))
 
+    @unittest.skipIf(not os.path.exists(API_FILE), 'No wunderground api key file found')
     def test_fail_autocomplete(self):
         self._proto.privmsg('tester', base.TEST_CHANNEL, '!weather aohicehcaoiehfoaiehfe')
         self.assertEqual(1, len(self._proto.msgs))
