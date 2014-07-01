c0df2cd8ff82db1f7b4dffe1a7587d5404e7afb8
Joshua Downer <joshua.downer@gmail.com>
2014-07-01 13:15:54 -0400

    wc: added world cup plugin

---
+++ plugins/wc.py
@@ -0,0 +1,24 @@
+import requests
+
+class WorldCup(Plugin):
+    def __init__(self, config={}):
+        super(WorldCup, self).__init__('wc')
+
+    @irc_command('Get current score from world cup')
+    def wc(self, user, channel, args):
+        try:
+            response = requests.get('http://worldcup.sfg.io/matches/current')
+            results = response.json()
+            for result in results:
+                away_team = result['away_team']['country']
+                home_team = result['home_team']['country']
+                away_goals = result['away_team']['goals']
+                home_goals = result['home_team']['goals']
+                msg = '%s %d-%d %s' % (
+                        home_team,
+                        home_goals,
+                        away_goals,
+                        away_team)
+                self._proto.send_msg(channel, msg)
+        except Exception as e:
+            self._proto.send_msg(channel, 'unable to retrieve result')
