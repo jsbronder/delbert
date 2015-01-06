5ab55f85a3481d42f7e32cbb09b2558d5c7236f4
Justin Bronder <jsbronder@gmail.com>
2015-01-05 23:11:44 -0500

    plugins:  add site down checker
    closes #11

---
+++ plugins/isitdown.py
@@ -0,0 +1,66 @@
+import requests
+from twisted.python import log
+
+class IsItDown(Plugin):
+    def __init__(self, config={}):
+        super(IsItDown, self).__init__('isitdown')
+        self._config = config
+
+    @staticmethod
+    def parse_site(url):
+        """
+        Strip the protocol and any path from a URL leaving only the site.
+
+        @param url  - url to strip
+        @return     - site contained within the url
+        """
+        site = url.strip()
+
+        if '://' in site:
+            site = site[site.find('://') + 3:]
+
+        if '/' in site:
+            site = site[:site.find('/')]
+
+        return site
+
+    def query(self, url):
+        """
+        Query the site checker
+
+        @return  - True if the site is up, False otherwise.
+        """
+        try:
+            html = requests.get(
+                    'http://downforeveryoneorjustme.com/%s' % (url,),
+                    verify=False)
+        except requests.exceptions.RequestException, e:
+            log.err(str(e))
+            return False
+
+        return 'not just you!' not in html.text
+
+    @irc_command('check if a site or sites are down for everyone')
+    def isitdown(self, user, channel, args):
+        if len(args):
+            urls = args.split(' ')
+
+            for url in urls:
+                site = self.parse_site(url)
+
+                up = self.query(site)
+                msg = '%s is %s' % (site, 'up' if up else 'down')
+
+                if channel == self.nickname:
+                    self._proto.send_msg(get_nick(user), msg)
+                else:
+                    self._proto.send_msg(channel, msg)
+        else:
+            msg = 'Check if what site or sites are down for everyone?'
+
+            if channel == self.nickname:
+                self._proto.send_msg(get_nick(user), msg)
+            else:
+                self._proto.send_msg(channel, msg)
+
+
---
+++ test/test_isitdown.py
@@ -0,0 +1,45 @@
+import unittest
+
+import base
+
+class IsItDownTester(unittest.TestCase):
+    def setUp(self):
+        self._plugin = base.load_plugin('isitdown.py', 'IsItDown')
+        self._proto = base.TestProto([self._plugin])
+
+    def test_parse(self):
+        site = self._plugin.parse_site('http://site.com')
+        self.assertEqual(site, 'site.com')
+
+        site = self._plugin.parse_site('https://site.com')
+        self.assertEqual(site, 'site.com')
+
+        site = self._plugin.parse_site('http://site.com/images')
+        self.assertEqual(site, 'site.com')
+
+        site = self._plugin.parse_site('site.com')
+        self.assertEqual(site, 'site.com')
+
+        site = self._plugin.parse_site('site.com/images')
+        self.assertEqual(site, 'site.com')
+
+    def test_query(self):
+        site = self._plugin.parse_site('http://downforeveryoneorjustme.com/')
+        up = self._plugin.query(site)
+        self.assertTrue(up)
+
+        up = self._plugin.query('sitedoesnotexist.com')
+        self.assertFalse(up)
+
+    def test_msg(self):
+        self._proto.privmsg('tester', base.TEST_CHANNEL, '!isitdown http://google.com/blah')
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertEqual(
+                self._proto.msgs[0][2],
+                'google.com is up')
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
