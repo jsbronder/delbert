ecc2907f37449906d7c3bf9b3370f6c9a541d368
Justin Bronder <jsbronder@gmail.com>
2015-03-10 13:35:29 -0400

    stocks:  add plugin to query stock quotes

---
+++ plugins/stocks.py
@@ -0,0 +1,57 @@
+import requests
+
+from twisted.python import log
+
+class InvalidSymbolError(Exception):
+    pass
+
+class Stocks(Plugin):
+    def __init__(self, config=None):
+        super(Stocks, self).__init__('stocks')
+        self._base_url = 'http://dev.markitondemand.com/Api/v2/'
+        self._config = config if config is not None else {}
+
+    def get_quote(self, symbol):
+        """
+        Get a stock quote for the specified symbol.  Return is a dictionary containing
+        the information documented at http://dev.markitondemand.com/#doc_quote
+
+        @param symbol   - symbol for which to lookup quote
+        @return         - stock quote
+        """
+        try:
+            url = self._base_url + 'Quote/json?symbol=%s' % (symbol,)
+            html = requests.get(url, verify=False)
+        except requests.exceptions.RequestException, e:
+            log.err(str(e))
+            return
+
+        ret = html.json()
+
+        if ret.get('Message', '').startswith('No symbol matches found for '):
+            raise InvalidSymbolError()
+
+        return html.json()
+
+    @irc_command('Lookup the current quote for the specified ticker symbol')
+    def quote(self, user, channel, args):
+        log.msg('Pulling quotes for symbols: %s' % (','.join(args.split())))
+
+        for arg in args.split():
+            try:
+                quote = self.get_quote(arg)
+            except InvalidSymbolError:
+                msg = 'Invalid Symbol %s' % (arg,)
+            else:
+                msg = '%s [%s], %.2f, %.2f(%.2f%%)' % (
+                        quote['Name'],
+                        quote['Symbol'],
+                        quote['LastPrice'],
+                        quote['Change'],
+                        quote['ChangePercent'])
+
+            if channel == self.nickname:
+                self._proto.send_msg(get_nick(user), msg)
+            else:
+                self._proto.send_notice(channel, msg)
+
---
+++ test/test_stocks.py
@@ -0,0 +1,58 @@
+import unittest
+
+import httpretty
+
+import base
+
+class StocksTester(unittest.TestCase):
+    def setUp(self):
+        self._stocks, self._exc = base.load_plugin_and_excs('stocks.py', 'Stocks')
+        self._proto = base.TestProto([self._stocks])
+
+    def test_quote(self):
+        m = self._stocks.get_quote('AAPL')
+        self.assertIn('Status', m)
+        self.assertEqual(m['Status'], 'SUCCESS')
+
+        for k in ('High', 'Name', 'LastPrice', 'Timestamp', 'Symbol', 'ChangePercent',
+                'Volume', 'ChangePercentYTD', 'MSDate', 'ChangeYTD', 'MarketCap',
+                'Open', 'Change', 'Low'):
+            self.assertIn(k, m)
+
+        with self.assertRaises(self._exc['InvalidSymbolError']):
+            m = self._stocks.get_quote('BLAH')
+
+    def test_msg(self):
+        body = '''{"Status":"SUCCESS",
+                    "Name":"Apple Inc",
+                    "Symbol":"AAPL",
+                    "LastPrice":124.31,
+                    "Change":-2.83,
+                    "ChangePercent":-2.22589271669026,
+                    "Timestamp":"Tue Mar 10 13:00:42 UTC-04:00 2015",
+                    "MSDate":42073.5421527937,
+                    "MarketCap":724074423880,
+                    "Volume":5845689,
+                    "ChangeYTD":110.38,
+                    "ChangePercentYTD":12.6200398622939,
+                    "High":127.21,
+                    "Low":123.8,
+                    "Open":126.62}'''
+        httpretty.enable()
+        httpretty.register_uri(
+                httpretty.GET,
+                'http://dev.markitondemand.com/Api/v2/Quote/json?symbol=AAPL',
+                body=body,
+                content_type='application/json')
+
+
+        self._proto.privmsg('tester', base.TEST_CHANNEL, '!quote AAPL')
+        self.assertEqual(1, len(self._proto.msgs))
+        self.assertEqual(self._proto.msgs[0][2], u'Apple Inc [AAPL], 124.31, -2.83(-2.23%)')
+        httpretty.disable()
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
