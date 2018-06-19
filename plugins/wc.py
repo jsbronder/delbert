1aea90a052ba223d9100a5a85988472ca31b7378
Joshua Downer <joshua.downer@gmail.com>
2018-06-19 14:51:06 -0400

    plugin: updated wc

--- plugins/wc.py
+++ plugins/wc.py
@@ -1,3 +1,5 @@
+import random
+
 import requests
 
 import delbert.plugin
@@ -9,19 +11,23 @@ class WorldCup(delbert.plugin.Plugin):
 
     @delbert.plugin.irc_command('Get current score from world cup')
     def wc(self, user, channel, args):
-        try:
-            response = requests.get('http://worldcup.sfg.io/matches/current')
-            results = response.json()
-            for result in results:
-                away_team = result['away_team']['country']
-                home_team = result['home_team']['country']
-                away_goals = result['away_team']['goals']
-                home_goals = result['home_team']['goals']
-                msg = '%s %d-%d %s' % (
-                        home_team,
-                        home_goals,
-                        away_goals,
-                        away_team)
-                self._proto.send_notice(channel, msg)
-        except Exception:
-            self._proto.send_notice(channel, 'unable to retrieve result')
+        if random.random() < 0.05:
+            notice = "Fuckin' calm down {}, it's soccer.".format(user)
+            self._proto.send_notice(channel, notice)
+        else:
+            try:
+                response = requests.get('http://worldcup.sfg.io/matches/current')
+                results = response.json()
+                for result in results:
+                    away_team = result['away_team']['country']
+                    home_team = result['home_team']['country']
+                    away_goals = result['away_team']['goals']
+                    home_goals = result['home_team']['goals']
+                    msg = '%s %d-%d %s' % (
+                            home_team,
+                            home_goals,
+                            away_goals,
+                            away_team)
+                    self._proto.send_notice(channel, msg)
+            except Exception:
+                self._proto.send_notice(channel, 'unable to retrieve result')
